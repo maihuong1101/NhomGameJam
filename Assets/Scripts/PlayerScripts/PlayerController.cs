ad6d2ceba75686b6c3426dd37f857bfa7b6ecd8d
phamvu03 <ptmvu03@gmail.com>
2025-01-18 02:23:09 +0700

    Adjust Player Script

--- Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using static UnityEngine.UI.Image;
 
 public class PlayerController : MonoBehaviour
 {
@@ -57,12 +58,20 @@ public class PlayerController : MonoBehaviour
             horizontalInput = Input.GetAxis("Horizontal");
         }
         // Set speed when player in the air
-        playerAnimation.SetFloat("AirSpeedY", playerRb.velocity.y);
+        //playerAnimation.SetFloat("AirSpeedY", playerRb.velocity.y);
+
+        if (isAlive)
+        {
+            Grounded();
+            Flip();
+            Move();
+            Jump();
+        }
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(groundCheckPoint.position + new Vector3(-0.05f, 0, 0) + 
+        Gizmos.DrawWireCube(groundCheckPoint.position + new Vector3(0f,0f,0f) + 
             Vector3.down * groundCheckDistance / 2, new Vector3(boxSize.x, boxSize.y, 1));
     }
     private void Flip()
@@ -77,5 +86,68 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector2(-1, transform.localScale.y);
             lookingRight = false;
         }
+    }
+    private void Move()
+    {
+        playerRb.velocity = new Vector2(horizontalInput * playerSpeed, playerRb.velocity.y);
+
+        //if (horizontalInput != 0)
+        //{
+        //    playerAnimation.SetFloat("Speed", 1);
+        //}
+        //else
+        //{
+        //    playerAnimation.SetFloat("Speed", 0);
+        //}
+    }
+    public bool IsOnGround()
+    {
+        if (Physics2D.BoxCast(groundCheckPoint.position, boxSize, 0, Vector2.down, groundCheckDistance, groundCheckLayer))
+        {
+            return true;
+        }
+        else return false;
+    }
+    private void Grounded()
+    {
+        if (IsOnGround())
+        {
+            //playerAnimation.SetBool("Grounded", true);
+            airJumpCounter = 0;
+            isJumping = false;
+        }
+        else
+        {
+            //playerAnimation.SetBool("Grounded", false);
+        }
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && playerRb.velocity.y > 0)
+        {
+            //playerAnimation.SetTrigger("Jump");
+            playerRb.velocity = new Vector2(playerRb.velocity.x, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
+        {
+            isJumping = true;
+            isGrounded = false;
+            //playerAnimation.SetTrigger("Jump");
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+        }
+        else if (!IsOnGround() && airJumpCounter < maxAirJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            airJumpCounter++;
+            //playerAnimation.SetTrigger("Jump");
+            playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+        }
+    }
+    IEnumerator Death()
+    {
+        isAlive = false;
+        //playerAnimation.SetTrigger("Death");
+        yield return new WaitForSeconds(0.5f);
+        //StartCoroutine(UIManager.Instance.ActiveDeathScreen());
     }
 }
